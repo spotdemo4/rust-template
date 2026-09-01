9d59b00422a97285d0af44270edcd4e53b0de0be
doctor <doctor@trev.xyz>
2026-09-01 22:02:45 +0000

    bump: v0.9.0 -> v0.9.1

--- flake.nix
+++ flake.nix
@@ -94,7 +94,7 @@
           default = pkgs.rustPlatform.buildRustPackage (
             final: with pkgs.lib; {
               pname = "rust-template";
-              version = "0.9.0";
+              version = "0.9.1";
 
               src = fileset.toSource {
                 root = ./.;
